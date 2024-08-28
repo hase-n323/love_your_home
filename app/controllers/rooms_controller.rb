43c1193900a9d1a4dc9ef1a926c20c55bc1b50d1
hase-n323 <hase.n0400323@gmail.com>
2024-08-28 18:47:34 +0900

    シュミレーションページのデプロイ再実行

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,7 @@
 class RoomsController < ApplicationController
   def top; end
+
+  def floor_simulator; end
+
+  def wallpaper_simulator; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'rooms#top'
 
+  get 'floor_simulator', to: 'rooms#floor_simulator'
+  get 'wallpaper_simulator', to: 'rooms#wallpaper_simulator'
+
 end
