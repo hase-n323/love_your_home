9795f06b2280a84b732068e98afd9a6e570b18e9
hase-n323 <hase.n0400323@gmail.com>
2024-09-10 23:26:40 +0900

    環境構築からやり直し〜tailwind.dartsassの挙動確認-3を元に戻した

--- db/migrate/20240909191430_sorcery_core.rb
+++ db/migrate/20240909191430_sorcery_core.rb
@@ -1,6 +1,6 @@
 class SorceryCore < ActiveRecord::Migration[7.2]
   def change
-    create_table_if_not_exists :users do |t|
+    create_table :users do |t|
       t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
