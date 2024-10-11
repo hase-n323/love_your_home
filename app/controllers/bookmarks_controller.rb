63960a611dd03b411534a7d65c35695d900c4476
hase-n323 <hase.n0400323@gmail.com>
2024-10-11 16:01:00 +0900

    bookmarks_indexの作成

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,20 @@
 class BookmarksController < ApplicationController
   before_action :require_login  # ログインしているユーザーのみがアクセス可能にする
-
+  
   def create
     # 既にブックマークがあるか確認
-    bookmark = current_user.bookmarks.find_or_initialize_by(product_number: params[:product_number])
-    bookmark.product_link = params[:product_link]
-    
-    if bookmark.save
+    bookmark = current_user.bookmarks.find_or_initialize_by(product_number: bookmark_params[:product_number])
+    if bookmark.update(bookmark_params)
       flash[:notice] = "ブックマークしました"
     else
       flash[:alert] = "ブックマークに失敗しました"
     end
     redirect_to simulator_path
+  end
+
+  # current_user のブックマークを取得し、ビューに渡している
+  def index
+    @bookmarks = current_user.bookmarks
   end
 
   def destroy
@@ -25,5 +28,9 @@ class BookmarksController < ApplicationController
     redirect_to simulator_path
   end
   
+  private  # クラス内に移動
+
+  def bookmark_params
+    params.require(:bookmark).permit(:product_number, :product_link, :product_image_url)
+  end
 end
-  
