5885138fac1f4cd3767ee3dc376d8d1a5294dd8d
hase-n323 <hase.n0400323@gmail.com>
2024-10-24 23:36:41 +0900

    環境変数の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               '***.onrender.com',
+    domain:               'love_your_home.onrender.com',
     user_name:            ENV['SMTP_USER_NAME'],
     password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
+    authentication:       'love_your_home',
     enable_starttls_auto: true 
   }
 
